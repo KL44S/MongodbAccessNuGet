4606cbf399d1b5207293c979d401584af15f3d10
KL44S <agustinbinci@hotmail.com>
2019-07-23 21:58:29 -0300

    constructor

--- MongodbAccess/Model/MongodbConfig.cs
+++ MongodbAccess/Model/MongodbConfig.cs
@@ -4,6 +4,11 @@
     {
         public string ConnectionString { get; set; }
         public string DBName { get; set; }
-    }
 
+        public MongodbConfig(string connectionString, string dbName)
+        {
+            this.ConnectionString = connectionString;
+            this.DBName = dbName;
+        }
+    }
 }

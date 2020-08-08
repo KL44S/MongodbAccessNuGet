3b8a8b871a5eded15183136bc3707f0d85661989
KL44S <agustinbinci@hotmail.com>
2020-08-08 16:40:59 -0300

    Performance tests

---
+++ MongodbAccess.Tests/Helpers/TestHelper.cs
@@ -0,0 +1,61 @@
+﻿using MongodbAccess.Tests.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MongodbAccess.Tests.Helpers
+{
+    public static class TestHelper
+    {
+        private static readonly Random Random = new Random();
+
+        public static IList<Test> CreateRandomTests(int testsNumber)
+        {
+            IList<Test> tests = new List<Test>();
+
+            for (int i = 0; i < testsNumber; i++)
+            {
+                Test test = CreateRandomTest();
+                tests.Add(test);
+            }
+
+            return tests;
+        }
+
+        private static Test CreateRandomTest()
+        {
+            Test test = new Test()
+            {
+                BoolField = CreateRandomBool(),
+                NumberField = Random.NextDouble(),
+                StringField = CreateRandomString(),
+                TimestampField = CreateRandomDatetime()
+            };
+
+            return test;
+        }
+
+        private static string CreateRandomString()
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            return new string(Enumerable.Repeat(chars, Random.Next(50))
+              .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        private static bool CreateRandomBool()
+        {
+            int probability = Random.Next(100);
+
+            return probability <= 20;
+        }
+
+        private static DateTime CreateRandomDatetime()
+        {
+            DateTime start = new DateTime(1995, 1, 1);
+            int range = (DateTime.Today - start).Days;
+
+            return start.AddDays(Random.Next(range));
+        }
+    }
+}
---
+++ MongodbAccess.Tests/PerformanceTests.cs
@@ -0,0 +1,63 @@
+﻿using MongoDB.Driver;
+using MongodbAccess.Implementations;
+using MongodbAccess.Model;
+using MongodbAccess.Services;
+using MongodbAccess.Tests.Helpers;
+using MongodbAccess.Tests.Models;
+using RepositoryAccess;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MongodbAccess.Tests
+{
+    public class PerformanceTests
+    {
+        [Fact]
+        public async Task InsertManyVsInsertOne()
+        {
+            MongodbConfig mongodbConfig = MongoDbHelper.GetMongodbConfig();
+            IMongoDatabase database = MongodbProvider.GetDatabase(mongodbConfig);
+            ISaveRepository<Test> saveRepository = new MongodbSaveRepository<Test>(database);
+            Stopwatch stopwatch = new Stopwatch();
+
+            IList<Test> tests = TestHelper.CreateRandomTests(100);
+
+            // First insert in order establish the connection
+            await saveRepository.SaveAsync(tests.ElementAt(0));
+
+            // One-by-one insert
+            stopwatch.Restart();
+            foreach (Test test in tests)
+            {
+                await saveRepository.SaveAsync(test);
+            }
+            stopwatch.Stop();
+            TimeSpan oneByOneTime = stopwatch.Elapsed;
+
+            // Multi-thread insert
+            stopwatch.Restart();
+            IList<Task> tasks = new List<Task>();
+            foreach (Test test in tests)
+            {
+                tasks.Add(saveRepository.SaveAsync(test));
+            }
+            await Task.WhenAll(tasks);
+            stopwatch.Stop();
+            TimeSpan multiThreadTime = stopwatch.Elapsed;
+
+            // Insert many (bulk)
+            stopwatch.Restart();
+            await saveRepository.SaveAsync(tests);
+            stopwatch.Stop();
+            TimeSpan bulkTime = stopwatch.Elapsed;
+
+            Assert.True(
+                TimeSpan.Compare(oneByOneTime, multiThreadTime) == 1 &&
+                TimeSpan.Compare(multiThreadTime, bulkTime) == 1);
+        }
+    }
+}
